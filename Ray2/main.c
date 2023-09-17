bba6783da2b331ed592328a7fb93ec4543b69113
NickyFranklin <nafrankl@mtu.edu>
2023-09-17 14:37:04 -0400

    Finsihed

--- Ray2/main.c
+++ Ray2/main.c
@@ -14,6 +14,87 @@ typedef struct {
   unsigned char r, g, b;
 } Pixel;
 
+typedef struct {
+  float pos[3];
+  float radius;
+} Sphere;
+
+float dotProduct(float x1, float y1, float z1, float x2, float y2, float z2) {
+  return (x1 * x2) + (y1 * y2) + (z1 * z2);
+}
+
+void subVector(float x1, float y1, float z1, float x2, float y2, float z2, float* resultX,
+		float* resultY, float* resultZ) {
+  *resultX = x1 - x2;
+  *resultY = y1 - y2;
+  *resultZ = z1 - z2;
+}
+
+//This assumes a 2 by 2 image grid
+void TwoDto3D (float* x, float* y, float* z, float* pixelX, float* pixelY) {
+  //current 2d coord / total 2d * total 3d
+  *x = ((*pixelX + 0.5) - 256) / 512 * 2;
+  *y = (((*pixelY + 0.5) - 256) / 256) * -1;
+  //lost scaling information
+  *z = -2;
+}
+
+//this assumes a 2 by 2 image grid
+void ThreeDto2D (float* x, float* y, float* z, float* pixelX, float* pixelY) {
+  //current (3d coord * image plane / z)/ total 3d coord * total 2d coord
+  float scaling = -2 / *z;
+  *pixelX = (((*x/2) * scaling) * 512 + 256);
+  *pixelY = (512 - (((*y/2) * scaling) *  512 + 256));
+}
+
+float Sphere_intersectT(Ray ray, Sphere sphere) {
+  float centerX;
+  float centerY;
+  float centerZ;
+  subVector(ray.pos[0], ray.pos[1], ray.pos[2], sphere.pos[0], sphere.pos[1], sphere.pos[2], &centerX,
+	    &centerY, &centerZ);
+  float discriminant;
+  float rayDirCentSquare = dotProduct(ray.dir[0], ray.dir[1], ray.dir[2], centerX, centerY, centerZ);
+  rayDirCentSquare = rayDirCentSquare * rayDirCentSquare;
+  float dirSquare = dotProduct(ray.dir[0], ray.dir[1], ray.dir[2], ray.dir[0], ray.dir[1], ray.dir[2]);
+  float radiusSquare = sphere.radius * sphere.radius;
+  float centerSquareRad = dotProduct(centerX, centerY, centerZ, centerX, centerY, centerZ);
+  centerSquareRad = centerSquareRad - radiusSquare;
+  discriminant = (rayDirCentSquare) - (dirSquare * centerSquareRad);
+  if(discriminant <= 0) {
+    return -1;
+  }
+
+  discriminant = sqrt(discriminant);
+  float outer = dotProduct(-1 * ray.dir[0], -1 * ray.dir[1], -1 * ray.dir[2],
+			   centerX, centerY, centerZ);
+  float t = (outer - discriminant) / dirSquare;
+  float otherT = (outer - discriminant) / dirSquare;
+  if(otherT < t && otherT > 0) {
+    t = otherT;
+  }
+  return t;
+}
+
+void getPixelIntersect(float t, float x, float y, float z, float* pixelX, float* pixelY) {
+  x = x * t;
+  y = y * t;
+  z = z * t;
+  ThreeDto2D(&x, &y, &z, pixelX, pixelY);
+}
+
+/*
+float dotProduct(float x1, float y1, float z1, float x2, float y2, float z2) {
+  return (x1 * x2) + (y1 * y2) + (z1 * z2);
+}
+
+void subVector(float x1, float y1, float z1, float x2, float y2, float z2, float* resultX,
+		float* resultY, float* resultZ) {
+  *resultX = x1 - x2;
+  *resultY = y1 - y2;
+  *resultZ = z1 - z2;
+}
+*/
 //Going to try to map 3d to 2d by dividing the distance to the object by z and then multiply x and y by z
 Ray getRay(float x, float y, float z, Ray ray) {
   for(int i = 0; i < 3; i++) {
@@ -33,11 +114,12 @@ void normalize(float *x, float *y, float *z) {
   *z = (*z)/norm;
 }
 
+/*
 //This assumes a 2 by 2 image grid
 void TwoDto3D (float* x, float* y, float* z, float* pixelX, float* pixelY) {
   //current 2d coord / total 2d * total 3d
-  *x = (*pixelX - 256) / 512 * 2;
-  *y = ((*pixelY - 256) / 256) * -1;
+  *x = ((*pixelX + 0.5) - 256) / 512 * 2;
+  *y = (((*pixelY + 0.5) - 256) / 256) * -1;
   //lost scaling information
   *z = -2;
 }
@@ -49,7 +131,7 @@ void ThreeDto2D (float* x, float* y, float* z, float* pixelX, float* pixelY) {
   *pixelX = (((*x/2) * scaling) * 512 + 256);
   *pixelY = (512 - (((*y/2) * scaling) *  512 + 256));
 }
-
+*/
 void printRay(Ray ray) {
   printf("RayPosition %.6f %.6f %.6f\n", ray.pos[0], ray.pos[1], ray.pos[2]);
   printf("RayDirection %.6f %.6f %.6f\n", ray.dir[0], ray.dir[1], ray.dir[2]);
@@ -95,22 +177,42 @@ int main(int argc, char *argv[]) {
 
   //making and allocating space for the array of pixels
   Pixel *pixelArr;
+  Ray *rayArr;
   pixelArr = (Pixel *) malloc((width * length) * sizeof(Pixel));
+  rayArr = (Ray *) malloc((width * length) * sizeof(Ray));
+  Pixel zeroPixel;
+  zeroPixel.r = 0;
+  zeroPixel.g = 0;
+  zeroPixel.b = 0;
+  Ray zeroRay;
+  zeroRay = getRay(0.0, 0.0, 0.0, zeroRay);
+  for(int i = 0; i < width * length; i++) {
+    pixelArr[i] = zeroPixel;
+    rayArr[i] = zeroRay;
+  }
+  
+  
   long size = width*length;
+  /*
   Ray testRay;
   testRay = getRay(-1.0, -1.0, -2.0, testRay);
   printRay(testRay);
   normalize(&testRay.dir[0], &testRay.dir[1], &testRay.dir[2]);
   printRay(testRay);
-
-  float pixelX = 0.5;
-  float pixelY = 0.5;
-  //float x = 0;
-  //float y = 0;
-  //float z = 0;
+  */
+  float pixelX = 0;
+  float pixelY = 0;
+  /*
   TwoDto3D(&testRay.dir[0], &testRay.dir[1], &testRay.dir[2], &pixelX, &pixelY);
   normalize(&testRay.dir[0], &testRay.dir[1], &testRay.dir[2]);
   printRay(testRay);
+
+  pixelX = 511;
+  pixelY = 511;
+  TwoDto3D(&testRay.dir[0], &testRay.dir[1], &testRay.dir[2], &pixelX, &pixelY);
+  normalize(&testRay.dir[0], &testRay.dir[1], &testRay.dir[2]);
+  printRay(testRay);
+  */
   
   //image plane is 2x2 for the assignment
   //float xInc = (float) 2 /(float) length;
@@ -125,8 +227,51 @@ int main(int argc, char *argv[]) {
     
   }
   */
+  Sphere sphere;
+  sphere.pos[0] = 2;
+  sphere.pos[1] = 2;
+  sphere.pos[2] = -16;
+  sphere.radius = 5.3547;
+
   
-  
-  
+  for(int y = 0; y < length; y++) {
+    for(int x = 0; x < width; x++) {
+      //printf("x: %d , y: %d\n", x, y);
+      int i  = x + (y * 512);
+      pixelX = (float) x;
+      pixelY = (float) y;
+      TwoDto3D(&rayArr[i].dir[0], &rayArr[i].dir[1], &rayArr[i].dir[2], &pixelX, &pixelY);
+      normalize(&rayArr[i].dir[0], &rayArr[i].dir[1], &rayArr[i].dir[2]);
+      if(x == 0 && y == 511) {
+	printf("Bottom left pixel\n");
+	printRay(rayArr[i]);
+      }
+      if(x == 511 && y == 0) {
+	printf("Top right pixel\n");
+	printRay(rayArr[i]);
+      }
+      if(x == 255 && y == 257) {
+	printf("Middle pixel\n");
+	printRay(rayArr[i]);
+      }
+      float t = Sphere_intersectT(rayArr[i], sphere);
+      if(t > 0) {
+	pixelArr[i].r = 255;
+	pixelArr[i].g = 255;
+	pixelArr[i].b = 255;
+      }
+      else {
+	pixelArr[i].r = 128;
+	pixelArr[i].g = 0;
+	pixelArr[i].b = 0;
+      }
+    }
+  }
+
+  FILE *fp;
+  fp = fopen("sphere.png", "w");
+  stbi_write_png("sphere.png", 512, 512, 3, pixelArr, 512*3);
+  fclose(fp);
+  return 0;
 }
 
