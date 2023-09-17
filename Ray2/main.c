fb2cf235bb1ca73f656b88a22100d1d7ad3b444a
NickyFranklin <nafrankl@mtu.edu>
2023-09-16 23:55:15 -0400

    ray direction done?

--- Ray2/main.c
+++ Ray2/main.c
@@ -5,15 +5,24 @@
 #include <math.h>
 
 
-struct Ray {
-  float position[3];
-  float direction[3];
-};
-typedef struct Ray Ray;
+typedef struct {
+  float pos[3];
+  float dir[3];
+} Ray;
+
+typedef struct {
+  unsigned char r, g, b;
+} Pixel;
 
 //Going to try to map 3d to 2d by dividing the distance to the object by z and then multiply x and y by z
-Ray getRay(float x, float y, float z) {
-  
+Ray getRay(float x, float y, float z, Ray ray) {
+  for(int i = 0; i < 3; i++) {
+    ray.pos[i] = 0;
+  }
+  ray.dir[0] = x;
+  ray.dir[1] = y;
+  ray.dir[2] = z;
+  return ray;
 }
 
 void normalize(float *x, float *y, float *z) {
@@ -24,13 +33,32 @@ void normalize(float *x, float *y, float *z) {
   *z = (*z)/norm;
 }
 
-void 2Dto3D (float* x, float* y, float* z, float* pixelX, float* pixelY) {
-  
+//This assumes a 2 by 2 image grid
+void TwoDto3D (float* x, float* y, float* z, float* pixelX, float* pixelY) {
+  //current 2d coord / total 2d * total 3d
+  *x = (*pixelX - 256) / 512 * 2;
+  *y = ((*pixelY - 256) / 256) * -1;
+  //lost scaling information
+  *z = -2;
+}
+
+//this assumes a 2 by 2 image grid
+void ThreeDto2D (float* x, float* y, float* z, float* pixelX, float* pixelY) {
+  //current (3d coord * image plane / z)/ total 3d coord * total 2d coord
+  float scaling = -2 / *z;
+  *pixelX = (((*x/2) * scaling) * 512 + 256);
+  *pixelY = (512 - (((*y/2) * scaling) *  512 + 256));
+}
+
+void printRay(Ray ray) {
+  printf("RayPosition %.6f %.6f %.6f\n", ray.pos[0], ray.pos[1], ray.pos[2]);
+  printf("RayDirection %.6f %.6f %.6f\n", ray.dir[0], ray.dir[1], ray.dir[2]);
 }
 
 //The coordinate functions only work with 512 pixels. Will add another parameter to the
 //function later to allow any resolution
 //As is self explanatory, they give me the coordinates of a pixel given a value
+/*
 long yCoord(int pixel) {
   pixel = pixel/3;
   return pixel / 512;
@@ -40,6 +68,7 @@ long xCoord(int pixel) {
   pixel = pixel/3;
   return pixel % 512;
 }
+*/
 
 int main(int argc, char *argv[]) {
   //We will need width and length for the window
@@ -65,11 +94,39 @@ int main(int argc, char *argv[]) {
   }
 
   //making and allocating space for the array of pixels
-  unsigned char *pixelArray;
-  pixelArray = (unsigned char*) malloc((width * length * 3) * sizeof(unsigned char));
-  long size = width*length*3;
+  Pixel *pixelArr;
+  pixelArr = (Pixel *) malloc((width * length) * sizeof(Pixel));
+  long size = width*length;
+  Ray testRay;
+  testRay = getRay(-1.0, -1.0, -2.0, testRay);
+  printRay(testRay);
+  normalize(&testRay.dir[0], &testRay.dir[1], &testRay.dir[2]);
+  printRay(testRay);
 
+  float pixelX = 0.5;
+  float pixelY = 0.5;
+  //float x = 0;
+  //float y = 0;
+  //float z = 0;
+  TwoDto3D(&testRay.dir[0], &testRay.dir[1], &testRay.dir[2], &pixelX, &pixelY);
+  normalize(&testRay.dir[0], &testRay.dir[1], &testRay.dir[2]);
+  printRay(testRay);
   
-
+  //image plane is 2x2 for the assignment
+  //float xInc = (float) 2 /(float) length;
+  //float yInc = (float) 2 / (float) width;
+  //printf("%.6f %.6f\n", xInc, yInc);
+  
+  /*
+  for(int i = 0; i < size; i++) {
+    pixelArr[i].r = 0;
+    pixelArr[i].g = 0;
+    pixelArr[i].b = 0;
+    
+  }
+  */
+  
+  
+  
 }
 
